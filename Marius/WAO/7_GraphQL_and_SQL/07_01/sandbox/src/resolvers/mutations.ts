deaae971dcff009e308aff9aa1be3348568f23db
MariusSB <93199243+MariusSB@users.noreply.github.com>
2024-02-19 12:02:02 +0100

    Surely

---
+++ Marius/WAO/7_GraphQL_and_SQL/07_01/sandbox/src/resolvers/mutations.ts
@@ -0,0 +1,16 @@
+import { MutationResolvers } from "__generated__/resolvers-types";
+
+// Use the generated `MutationResolvers` type to type check our mutations!
+const mutations: MutationResolvers = {
+  // Below, we mock adding a new book. Our data set is static for this
+  // example, so we won't actually modify our data.
+  addBook: async (_, { title, author }, { dataSources }) => {
+    return dataSources.booksAPI.addBook({ title, author });
+  },
+
+  addUser: async (_, { username, password }, { dataSources }) => {
+    return dataSources.usersAPI.addUser({ username, password });
+  },
+};
+
+export default mutations;
