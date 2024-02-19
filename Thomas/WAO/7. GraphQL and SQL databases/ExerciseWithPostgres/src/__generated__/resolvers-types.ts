00335aff3048eb461af06d7311f02527534f9671
Thomas Kaae <kaaenthom@gmail.com>
2024-02-19 13:26:16 +0100

    Exercise 2 (postgres without prisma)

---
+++ Thomas/WAO/7. GraphQL and SQL databases/ExerciseWithPostgres/src/__generated__/resolvers-types.ts
@@ -0,0 +1,261 @@
+import { GraphQLResolveInfo } from 'graphql';
+import { MyContext } from '../index';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type AddBookMutationResponse = {
+  __typename?: 'AddBookMutationResponse';
+  book?: Maybe<Book>;
+  code: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+  success: Scalars['Boolean']['output'];
+};
+
+export type Book = {
+  __typename?: 'Book';
+  author?: Maybe<Scalars['String']['output']>;
+  title?: Maybe<Scalars['String']['output']>;
+};
+
+export type LoginResponse = {
+  __typename?: 'LoginResponse';
+  code: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+  success: Scalars['Boolean']['output'];
+  token?: Maybe<Scalars['String']['output']>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  addBook?: Maybe<AddBookMutationResponse>;
+  registerUser?: Maybe<RegisterUserMutationResponse>;
+};
+
+
+export type MutationAddBookArgs = {
+  author?: InputMaybe<Scalars['String']['input']>;
+  title?: InputMaybe<Scalars['String']['input']>;
+};
+
+
+export type MutationRegisterUserArgs = {
+  input: RegisterUserInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  books?: Maybe<Array<Maybe<Book>>>;
+  login?: Maybe<LoginResponse>;
+  user?: Maybe<User>;
+  users?: Maybe<Array<Maybe<User>>>;
+};
+
+
+export type QueryLoginArgs = {
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+};
+
+
+export type QueryUserArgs = {
+  token: Scalars['String']['input'];
+};
+
+export type RegisterUserInput = {
+  email: Scalars['String']['input'];
+  name: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+};
+
+export type RegisterUserMutationResponse = {
+  __typename?: 'RegisterUserMutationResponse';
+  code: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+  success: Scalars['Boolean']['output'];
+  user?: Maybe<User>;
+};
+
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String']['output'];
+  hashedPassword: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  token: Scalars['String']['output'];
+};
+
+export type WithIndex<TObject> = TObject & Record<string, any>;
+export type ResolversObject<TObject> = WithIndex<TObject>;
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = ResolversObject<{
+  AddBookMutationResponse: ResolverTypeWrapper<AddBookMutationResponse>;
+  Book: ResolverTypeWrapper<Book>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
+  ID: ResolverTypeWrapper<Scalars['ID']['output']>;
+  LoginResponse: ResolverTypeWrapper<LoginResponse>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Query: ResolverTypeWrapper<{}>;
+  RegisterUserInput: RegisterUserInput;
+  RegisterUserMutationResponse: ResolverTypeWrapper<RegisterUserMutationResponse>;
+  String: ResolverTypeWrapper<Scalars['String']['output']>;
+  User: ResolverTypeWrapper<User>;
+}>;
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = ResolversObject<{
+  AddBookMutationResponse: AddBookMutationResponse;
+  Book: Book;
+  Boolean: Scalars['Boolean']['output'];
+  ID: Scalars['ID']['output'];
+  LoginResponse: LoginResponse;
+  Mutation: {};
+  Query: {};
+  RegisterUserInput: RegisterUserInput;
+  RegisterUserMutationResponse: RegisterUserMutationResponse;
+  String: Scalars['String']['output'];
+  User: User;
+}>;
+
+export type AddBookMutationResponseResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['AddBookMutationResponse'] = ResolversParentTypes['AddBookMutationResponse']> = ResolversObject<{
+  book?: Resolver<Maybe<ResolversTypes['Book']>, ParentType, ContextType>;
+  code?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  success?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type BookResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['Book'] = ResolversParentTypes['Book']> = ResolversObject<{
+  author?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  title?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type LoginResponseResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['LoginResponse'] = ResolversParentTypes['LoginResponse']> = ResolversObject<{
+  code?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  success?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  token?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type MutationResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
+  addBook?: Resolver<Maybe<ResolversTypes['AddBookMutationResponse']>, ParentType, ContextType, Partial<MutationAddBookArgs>>;
+  registerUser?: Resolver<Maybe<ResolversTypes['RegisterUserMutationResponse']>, ParentType, ContextType, RequireFields<MutationRegisterUserArgs, 'input'>>;
+}>;
+
+export type QueryResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
+  books?: Resolver<Maybe<Array<Maybe<ResolversTypes['Book']>>>, ParentType, ContextType>;
+  login?: Resolver<Maybe<ResolversTypes['LoginResponse']>, ParentType, ContextType, RequireFields<QueryLoginArgs, 'email' | 'password'>>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'token'>>;
+  users?: Resolver<Maybe<Array<Maybe<ResolversTypes['User']>>>, ParentType, ContextType>;
+}>;
+
+export type RegisterUserMutationResponseResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['RegisterUserMutationResponse'] = ResolversParentTypes['RegisterUserMutationResponse']> = ResolversObject<{
+  code?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  success?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type UserResolvers<ContextType = MyContext, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = ResolversObject<{
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  hashedPassword?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  token?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type Resolvers<ContextType = MyContext> = ResolversObject<{
+  AddBookMutationResponse?: AddBookMutationResponseResolvers<ContextType>;
+  Book?: BookResolvers<ContextType>;
+  LoginResponse?: LoginResponseResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  RegisterUserMutationResponse?: RegisterUserMutationResponseResolvers<ContextType>;
+  User?: UserResolvers<ContextType>;
+}>;
+
---
+++ Thomas/WAO/7. GraphQL and SQL databases/ExerciseWithPostgres/src/datasources.ts
@@ -0,0 +1,97 @@
+import {
+  AddBookMutationResponse,
+  Book,
+  LoginResponse,
+  RegisterUserMutationResponse,
+  User,
+} from "./__generated__/resolvers-types";
+import { authenticate } from "./resolvers/authutils.js";
+import pkg from "pg";
+
+const { Pool } = pkg;
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: "mypassword",
+});
+
+export class UserDataSource {
+  async getUsers() {
+    const query = "SELECT * FROM users";
+    const { rows } = await pool.query(query);
+    return rows;
+  }
+
+  async getUserByToken(token) {
+    const query = {
+      text: "SELECT * FROM users WHERE token = $1",
+      values: [token],
+    };
+
+    const { rows } = await pool.query(query);
+    return rows[0];
+  }
+
+  async login(email: string, password: string): Promise<LoginResponse> {
+    const query = {
+      text: "SELECT * FROM users WHERE email = $1 AND hashed_password = $2",
+      values: [email, password],
+    };
+    const { rows } = await pool.query(query);
+    const user = rows[0];
+
+    if (!user) {
+      return {
+        code: "400",
+        success: false,
+        message: "User not found!",
+      };
+    }
+
+    return await authenticate(user.id, password, user.hashed_password);
+  }
+
+  async registerUser(user: User): Promise<RegisterUserMutationResponse> {
+    const { name, email, hashedPassword, token } = user;
+    const query = {
+      text: "INSERT INTO users(name, email, hashed_password, token) VALUES($1, $2, $3,  $4) RETURNING *",
+      values: [name, email, hashedPassword, token],
+    };
+    const { rows } = await pool.query(query);
+    return rows[0];
+  }
+}
+export class BooksDataSource {
+  async getBooks(): Promise<Book[]> {
+    const query = "SELECT * FROM books";
+    const { rows } = await pool.query(query);
+    return rows;
+  }
+
+  async addBook(book: Book): Promise<AddBookMutationResponse> {
+    const { title, author } = book;
+    const query = {
+      text: "INSERT INTO books(title, author) VALUES($1, $2) RETURNING *",
+      values: [title, author],
+    };
+    const { rows } = await pool.query(query);
+    const newBook = rows[0];
+
+    if (newBook) {
+      return {
+        code: "200",
+        success: true,
+        message: "New book added!",
+        book: newBook,
+      };
+    } else {
+      return {
+        code: "400",
+        success: false,
+        message: "Failed to add book",
+        book: null,
+      };
+    }
+  }
+}
