e2d90d23c33efa8257db54c63e9d4b57dfecbec4
Thomas Kaae <kaaenthom@gmail.com>
2024-02-22 14:04:05 +0100

    Added nodejs client to connect to c# server

---
+++ Thomas/WAO/8. Remote procedure calls (gRPC)/Exercise1+2/node/greeter_client_dotnet.js
@@ -0,0 +1,44 @@
+var PROTO_PATH = "protos/greet.proto";
+
+var parseArgs = require("minimist");
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).greeter;
+
+function main() {
+  var argv = parseArgs(process.argv.slice(2), {
+    string: "target",
+  });
+  var target;
+  if (argv.target) {
+    target = argv.target;
+  } else {
+    target = "localhost:5288";
+  }
+  var client = new hello_proto.Greeter(
+    target,
+    grpc.credentials.createInsecure()
+  );
+  var user;
+  if (argv._.length > 0) {
+    user = argv._[0];
+  } else {
+    user = "world";
+  }
+  client.sayHello({ name: user }, function (err, response) {
+    if (err) {
+      console.error("Error:", err);
+    } else {
+      console.log("Greeting:", response.message);
+    }
+  });
+}
+
+main();
